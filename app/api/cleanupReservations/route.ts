1d84392222a8f9ba4dedfa72a7fc2b1738b56127
silvio <silviongougni139@gmail.com>
2025-04-17 18:27:31 +0200

    Optimisation et correction des routes API

--- app/api/cleanupReservations/route.ts
+++ app/api/cleanupReservations/route.ts
@@ -1,47 +1,44 @@
 import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
-import dayjs from 'dayjs';
 
 export async function DELETE(request: Request) {
     try {
-        const now = dayjs()
+        // Utiliser new Date() directement
+        const now = new Date();
 
         const expiredReservations = await prisma.reservation.findMany({
-            where : {
+            where: {
                 OR: [
                     {
-                        reservationDate : {
-                            lt:now.format('DD/MM/YYYY')
-                        }
+                        reservationDate: {
+                            lt: now.toISOString().split('T')[0], // Format YYYY-MM-DD
+                        },
                     },
                     {
-                        reservationDate : {
-                            equals:now.format('DD/MM/YYYY')
+                        reservationDate: {
+                            equals: now.toISOString().split('T')[0], // Format YYYY-MM-DD
                         },
-                        endTime : {
-                            lt: now.format('HH:mm')
-                        }
+                        endTime: {
+                            lt: now.toTimeString().split(' ')[0], // Format HH:MM:SS
+                        },
+                    },
+                ],
+            },
+        });
 
-                    }
-             
-                ]
-            }
-        })
-
-        if(expiredReservations.length > 0){
+        if (expiredReservations.length > 0) {
             await prisma.reservation.deleteMany({
-                where : {
-                    id : {
-                        in : expiredReservations.map((reservation) => reservation.id)
-                    }
-                }
-            })
+                where: {
+                    id: {
+                        in: expiredReservations.map((reservation) => reservation.id),
+                    },
+                },
+            });
         }
-        
-    return NextResponse.json({ message: 'Expired reservations cleaned up' });
- 
+
+        return NextResponse.json({ message: 'Expired reservations cleaned up' });
     } catch (error) {
         console.error('Error cleaning up reservations:', error);
         return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-      }
+    }
 }
